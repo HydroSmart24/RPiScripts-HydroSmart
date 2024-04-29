43a7906b67d870ce06a21b8ceac4055732316cf7
Sandali Disara <sandalidissanayake123@gmail.com>
2024-04-30 00:06:33 +0530

    refine the code

--- SensorScripts/ultrasonic.py
+++ SensorScripts/ultrasonic.py
@@ -24,7 +24,7 @@ GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
 
 def get_distance():
-    # Set trigger to HIGH for 10 microseconds
+
     GPIO.output(TRIG, True)
     time.sleep(0.00001)
     GPIO.output(TRIG, False)
@@ -32,15 +32,12 @@ def get_distance():
     start_time = time.time()
     stop_time = time.time()
 
-    # Save start time
     while GPIO.input(ECHO) == 0:
         start_time = time.time()
 
-    # Save stop time
     while GPIO.input(ECHO) == 1:
         stop_time = time.time()
 
-    # Calculate elapsed time
     elapsed_time = stop_time - start_time
 
     # Calculate distance (in cm)
@@ -49,42 +46,32 @@ def get_distance():
     return distance
 
 def event_trigger(threshold_distance):
-    """
-    Implement event-based trigger system.
-    """
+   
     last_distance = None
 
     try:
         while True:
-            # Get current distance
+         
             current_distance = get_distance()
 
-            # Check if last_distance is None
             if last_distance is None:
                 last_distance = current_distance
-                continue  # Skip comparison in the first iteration
+                continue  
 
-            # Calculate the difference in distance
             distance_difference = abs(current_distance - last_distance)
 
-            # If the difference is less than or equal to 45.00 cm, continue to the next iteration
             if distance_difference <= threshold_distance:
                 continue
 
-            # Otherwise, print the actual difference
             print("Distance changed by {:.2f} cm".format(distance_difference))
-            
             last_distance = current_distance
-
-            # Sleep for a short interval before taking the next measurement
-            time.sleep(1)  # Adjust as needed
+            time.sleep(1)  
 
     except KeyboardInterrupt:
         # Clean up GPIO
         GPIO.cleanup()
 
 # Define threshold distance for event triggering
-threshold_distance = 45.0  # Adjust as needed
+threshold_distance = 45.0  
 
-# Start event trigger system
 event_trigger(threshold_distance)
